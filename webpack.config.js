787e15b71c8b097826cb6acea86c79c08ecc4bf1
amaldinesh7 <amalkdinesh@gmail.com>
2021-12-17 16:00:15 +0530

    Updated webpack config and dependecies

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,42 @@
-const PeerDepsExternalsPlugin = require("@bigbinary/peer-deps-externals-webpack-plugin");
+const PeerDepsExternalsPlugin = require("peer-deps-externals-webpack-plugin");
 const path = require("path");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
-  entry: {
-    index: "./src/components/index.js",
-  },
-  plugins: [new PeerDepsExternalsPlugin(), new BundleAnalyzerPlugin()],
-
-  module: {
-    rules: [
-      {
-        test: /\.md$/i,
-        use: "raw-loader",
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        include: [path.resolve(__dirname, "src")],
-        use: [
-          {
+module.exports = [
+  {
+    entry: "./index.js",
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
             loader: "babel-loader",
           },
-        ],
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
-          },
-        ],
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        use: [
-          {
-            loader: "file-loader",
-          },
-        ],
-      },
-    ],
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+            },
+          ],
+        },
+        {
+          test: /\.(png|jpe?g|gif|svg)$/i,
+          use: [
+            {
+              loader: "file-loader",
+            },
+          ],
+        },
+      ],
+    },
+    output: {
+      path: __dirname + "/dist",
+      filename: "index.js",
+      library: "neeto-team-members",
+      libraryTarget: "umd",
+    },
+    plugins: [new PeerDepsExternalsPlugin()],
   },
-  output: {
-    publicPath: "",
-    path: __dirname + "/dist",
-    filename: "index.js",
-    library: "neeto-team-member",
-    libraryTarget: "umd",
-  },
-};
+];
